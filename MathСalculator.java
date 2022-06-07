4e9969bead944be78871f0bac1f6ac57c0787503
tanavovk <tatiavovk@gmail.com>
2022-06-07 19:32:25 +0300

    HWtolesson9

---
+++ MathСalculator.java
@@ -0,0 +1,19 @@
+package hillel.lesson9;
+
+public abstract class MathСalculator {
+    // Add numbers
+    public int Add (int a, int b) {
+        int Sum;
+        Sum = a + b;
+        return Sum ;
+    }
+
+    // Multiplication of numbers
+    public int calculations (int d, int c) {
+        int quantity;
+        quantity = d * c;
+        return quantity ;
+    }
+
+}
+
