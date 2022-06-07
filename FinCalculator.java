3189310d8e5bc8057a7e33027da33788d5231284
tanavovk <tatiavovk@gmail.com>
2022-06-07 19:32:25 +0300

    HWtolesson9

---
+++ FinCalculator.java
@@ -0,0 +1,15 @@
+package hillel.lesson9;
+
+public class FinCalculator extends IngCalculator {
+
+    //percentage calculation
+
+    public int calculations(int a) {
+
+        int percent;
+        percent = a * 100;
+        return percent;
+    }
+
+
+}
---
+++ IngCalculator.java
@@ -0,0 +1,15 @@
+package hillel.lesson9;
+
+public class IngCalculator extends MathСalculator {
+    @Override
+
+    // Raising a number X to the power y
+
+    public int calculations (int a, int b) {
+
+        int value;
+        value = a ^ b;
+        return value;
+    }
+
+}
